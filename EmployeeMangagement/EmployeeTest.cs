e19aa66ccf0442dacc96abb83ea9b268c9e8efaa
Avadhut Balaso Pawar <avadhutpawar721@gmail.com>
2023-01-05 14:22:06 +0530

    DataSource

--- EmployeeMangagement/EmployeeTest.cs
+++ EmployeeMangagement/EmployeeTest.cs
@@ -1,13 +1,59 @@
-﻿using System;
+﻿using EmployeeManagement.Utilities;
+using EmployeeMangagement.Base;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeMangagement
+
+
+namespace EmployeeManagement
 {
-   public class EmployeeTest
+    public class EmployeeTest : AutomationWrapper
     {
+        [Test, TestCaseSource(typeof(DataSource), nameof(DataSource.validEmployeeData))]
+        public void AddValidEmployeeTest(string username, string password, string firstName, string middleName, string lastName, string pathVariable, string expectedResult)
+        {
+
+
+
+            // Given the username and password credentials 
+            driver.FindElement(By.Name("username")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+
+
+
+            //Login in to the system
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+
+
+            //Steps to add the new Employee
+            driver.FindElement(By.XPath("//span[text()='PIM']")).Click();
+            driver.FindElement(By.LinkText("Add Employee")).Click();
+            driver.FindElement(By.Name("firstName")).SendKeys(firstName);
+            driver.FindElement(By.Name("middleName")).SendKeys(middleName);
+            driver.FindElement(By.Name("lastName")).SendKeys(lastName);
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+
+
+            //The Verification for the Employee Name given the Xpath from validLoginData method
+            string nameValidation = driver.FindElement(By.XPath(pathVariable)).Text;
+            Assert.That(nameValidation.Contains(expectedResult));
+
+
+
+            // Print the Employee name(Not required)
+            Console.WriteLine(nameValidation);
+
+
+
+        }
+
+
 
     }
 }
--- EmployeeMangagement/Utilities/DataSource.cs
+++ EmployeeMangagement/Utilities/DataSource.cs
@@ -1,12 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeMangagement.Utilities
+
+
+namespace EmployeeManagement.Utilities
 {
-    internal class DataSource
+    public class DataSource
     {
+        public static object[] InvalidLoginData()
+        {
+            string[] dataset1 = new string[3];
+            dataset1[0] = "John";
+            dataset1[1] = "john123";
+            dataset1[2] = "Invalid credentials";
+
+
+
+
+            string[] dataset2 = new string[3];
+            dataset2[0] = "peter";
+            dataset2[1] = "peter123";
+            dataset2[2] = "Invalid credentials";
+
+
+
+
+            string[] dataset3 = new string[3];
+            dataset3[0] = "saul";
+            dataset3[1] = "saul123";
+            dataset3[2] = "Invalid credentials";
+
+
+
+            object[] allDataSet = new object[3];
+            allDataSet[0] = dataset1;
+            allDataSet[1] = dataset2;
+            allDataSet[2] = dataset3;
+            return allDataSet;
+        }
+
+
+
+        public static object[] validEmployeeData()
+        {
+            string[] dataset1 = new string[7];
+            dataset1[0] = "Admin";
+            dataset1[1] = "admin123";
+            dataset1[2] = "john";
+            dataset1[3] = "w";
+            dataset1[4] = "wick";
+            dataset1[5] = "//h6[text()='john wick']";
+            dataset1[6] = "john wick";
+
+
+
+
+
+            string[] dataset2 = new string[7];
+            dataset2[0] = "Admin";
+            dataset2[1] = "admin123";
+            dataset2[2] = "saul";
+            dataset2[3] = "g";
+            dataset2[4] = "goodman";
+            dataset2[5] = "//h6[text()='saul goodman']";
+            dataset2[6] = "saul goodman";
+
+
+
+
+            object[] allDataSet = new object[2];
+            allDataSet[0] = dataset1;
+            allDataSet[1] = dataset2;
+
+
+
+            return allDataSet;
+
+
+
+        }
     }
 }
